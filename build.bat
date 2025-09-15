d8faca9750cec3b5c5284372d99e11880b3ea5ee
potakusan <msqkn310@gmail.com>
2025-09-15 14:08:17 +0900

    1

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-node ./main.js build --n release.json --o 20241103 --r 87
+node ./main.js build --n 20250915.json --o 20250915 --r 87
 pause
--- src/build.js
+++ src/build.js
@@ -1,53 +1,56 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class Build {
-
-  constructor(options){
-    this.outputName = options.n
-    this.outputVersion = options.o || this.getDate()
-    this.requireVersion = options.r || "1"
-    return this
+  constructor(options) {
+    this.outputName = options.n;
+    this.outputVersion = options.o || this.getDate();
+    this.requireVersion = options.r || "1";
+    return this;
   }
 
-  getDate(){
-    const d = new Date()
-    return String(d.getFullYear()) + String("0" + d.getMonth() + 1).slice(-2) + String(d.getDate())
+  getDate() {
+    const d = new Date();
+    return (
+      String(d.getFullYear()) +
+      String("0" + d.getMonth() + 1).slice(-2) +
+      String(d.getDate())
+    );
   }
 
-  getFile(fileName){
-    return fs.readFileSync(`./input/${fileName}.json`, 'utf-8')
+  getFile(fileName) {
+    return fs
+      .readFileSync(`./input/${fileName}.json`, "utf-8")
+      .replace(/^\uFEFF/, "");
   }
 
-  writeFile(output){
-    const outDir = `./output/${this.outputName}`
-    fs.writeFileSync(outDir,JSON.stringify(output))
-    console.log("Successfully finished! - " + outDir)
+  writeFile(output) {
+    const outDir = `./output/${this.outputName}`;
+    fs.writeFileSync(outDir, JSON.stringify(output));
+    console.log("Successfully finished! - " + outDir);
     return true;
   }
 
-  async exec(){
-    const inputs = ["sp11","sp12","dp11","dp12"]
-    let body = []
-    try{
-      for(let i = 0; i < inputs.length; ++i){
-        const res = await this.getFile(inputs[i])
-        JSON.parse(res).map((item)=>{
-          body.push(item)
-        })
+  async exec() {
+    const inputs = ["sp11", "sp12", "dp11", "dp12"];
+    let body = [];
+    try {
+      for (let i = 0; i < inputs.length; ++i) {
+        const res = this.getFile(inputs[i]);
+        console.log(res);
+        JSON.parse(res).map((item) => {
+          body.push(item);
+        });
       }
-      return this.writeFile(
-        {
-          "version":this.outputVersion,
-          "requireVersion":String(this.requireVersion),
-          "body":body
-        }
-      )
-    }catch(e){
-      console.error(e)
-      return false
+      return this.writeFile({
+        version: this.outputVersion,
+        requireVersion: String(this.requireVersion),
+        body: body,
+      });
+    } catch (e) {
+      console.error(e);
+      return false;
     }
   }
-
 }
 
-module.exports = Build
+module.exports = Build;
